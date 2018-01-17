daad08968cc2ca01adb0468f93de506f20f4d5ab
Parminder Singh <psingh35@mail.greenriver.edu>
2018-01-17 03:02:21 -0500

    added Sort to array

--- functionarray.php
+++ functionarray.php
@@ -1,1 +1,11 @@
 <?php
+$animals = array("panda", "aplaca", "boa");
+
+function animalsList($animals)
+{
+    sort($animals);
+    //count the animals
+    foreach ($animals as $eachAnimal)
+    { echo $eachAnimal.' ';}
+    echo "<br>";
+}
