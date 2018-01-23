90a00711c9f6e484f04a99ddfbe22d13aeb9255a
Parminder Singh <psingh35@mail.greenriver.edu>
2018-01-23 18:19:48 -0500

    added files, Had issue with addig commits before. Layout made

--- functionarray.php
+++ functionarray.php
@@ -9,3 +9,30 @@ function animalsList($animals)
     { echo $eachAnimal.' ';}
     echo "<br>";
 }
+function add($animals, $item) {
+    echo "adding $item..<br>";
+    $convStr = strtolower($item);
+    if(!in_array($convStr, $animals)) {
+        array_push($animals, $convStr);
+    }
+    return $animals;
+}
+animalsList($animals);
+$animals = add($animals, 'goat');
+animalsList($animals);
+$animals = add($animals, 'Boa');
+animalsList($animals);
+
+echo "<br>";
+
+$cupcakeFlav = array("grasshopper"=>"The Grasshopper","maple"=>"Whiskey Maple Bacon",
+    "carrot"=>"Carrot Walnut","caramel"=>"Salted Caramel Cupcake","velvet"=>"Red Velvet",
+    "lemon"=>"Lemon Drop","tiramisu"=>"Tiramisu");
+
+function flavors($flav){
+    foreach ($flav as $k => $v){
+        echo "<input type='checkbox' name='flavors[]' value='$k'> $v<br>";
+    }
+}
+
+flavors($cupcakeFlav);
